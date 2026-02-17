acf24433404951204ba51ab283d1a4f7d99a4cb1
“surjeet” <surjeetjothi@gmail.com>
2026-02-17 20:39:32 +0530

    Update frontend folders

---
+++ Classbridge/class/frontend/nextjs_app/app/static-app/page.tsx
@@ -0,0 +1,8 @@
+export default function StaticAppPage() {
+    return (
+        <iframe
+            src="/static_app/index.html"
+            style={{ width: "100%", height: "100vh", border: "none" }}
+        />
+    );
+}
